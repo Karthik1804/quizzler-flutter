a2f86e9415be3b356c1f013843580db7dd5426c5
Karthik <karthik180499@gmail.com>
2020-06-14 22:44:13 +0530

    Initial commit

---
+++ Apps/2/i_am_poor/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Poor'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/poor.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
---
+++ Apps/6/quizzler-flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Sofware\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Karthik_Personal\Karthik_Training\Udemy\The Complete 2020 Flutter Development Bootcamp with Dart\Apps\6\quizzler-flutter"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Sofware\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
